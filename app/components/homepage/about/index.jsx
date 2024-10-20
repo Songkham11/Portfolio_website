e6d1d70c4dfb81d63b2cef272dc55309b0ca00fd
Tony Wilson <unityengineer0101@gmail.com>
2024-10-21 02:23:07 +0700

    change_root

--- app/components/homepage/about/index.jsx
+++ app/components/homepage/about/index.jsx
@@ -24,7 +24,7 @@ function AboutSection() {
         </div>
         <div className="flex justify-center order-1 lg:order-2">
           <Image
-            src={`profile`}
+            src={profile}
             width={280}
             height={280}
             alt="tony wilson"
