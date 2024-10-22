3652be283e1992d8e346c3ede688351d7fafcb96
Tony Wilson <unityengineer0101@gmail.com>
2024-10-22 21:33:31 +0700

    gmail change

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -3,12 +3,12 @@ export const personalData = {
   profile: '/public/svg/skills/profile.png',
   designation: "Software Engineer",
   description: "Innovative Full-Stack Developer with 8+ years of experience, specializing in smart contracts and web3 technologies. Adept at designing and implementing scalable applications and solutions, delivering high-quality results in collaborative environments.",
-  email: 'unityengineer0101@gmail.com',
+  email: 'superdev0605@gmail.com',
   phone: '+ 856 20940 90645',
   github: 'https://github.com/Songkham11',
-  telegram: 'https://t.me/unityengineer0101',
-  twitter: 'https://x.com/unityengineer0101',
-  leetcode: "https://leetcode.com/unityengineer0101",
-  devUsername: "unityengineer0101",
-  resume: "https://drive.google.com/file/d/1_tg8_I7BvajOC_QNjrmu1CqLeKZVaCSn/view?usp=sharing"
+  telegram: 'https://t.me/superdev0605',
+  twitter: 'https://x.com/superdev0605',
+  leetcode: "https://leetcode.com/superdev0605",
+  devUsername: "superdev0605",
+  resume: "https://drive.google.com/file/d/1pdskptPFmaW-0SnmDa0Z6lz4e8WCkC4V/view?usp=sharing"
 }
